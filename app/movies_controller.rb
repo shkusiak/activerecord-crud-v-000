3780061e56dc426a49ab65cad440b39e3feb30e1
Shea Kusiak <shkusiak@gmail.com>
2018-07-03 23:21:28 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -21,6 +21,7 @@ def can_be_created_with_a_hash_of_attributes
   }
   movie = Movie.new(attributes)
   movie.save
+  movie
 end
 
 def can_be_created_in_a_block(args = { title: "The Room", release_date: 2003 })
